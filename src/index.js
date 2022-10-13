7ee4a0d1802d244a2a2a90d2e0ce4c744fb61475
Adnan Alam <adnanalamcie@gmail.com>
2022-10-13 16:19:56 +0600

    react context added

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { UserProvider } from "./contexts/user.context";
 import "./index.scss";
 
 const rootElement = document.getElementById("root");
@@ -9,7 +10,9 @@ const rootElement = document.getElementById("root");
 render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <UserProvider>
+        <App />
+      </UserProvider>
     </BrowserRouter>
   </React.StrictMode>,
   rootElement
