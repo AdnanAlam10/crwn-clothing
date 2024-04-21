7282a7ef00aac7f2201f6f7e751250961fa70879
Adnan Alam <adnanalamcie@gmail.com>
2024-04-21 11:37:51 +1000

    Minor fixes

--- src/routes/home/home.jsx
+++ src/routes/home/home.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import Directory from "../../components/directory/directory";
+import Directory from "../../components/directory/directory.jsx";
 
 function Home() {
   return (
